97ee557912e5a21217119597e0e481cb27163889
Maksim Nikolaev <mksniklaev@gmail.com>
2023-10-21 15:32:20 +0500

    fix: убрал рамку с видео

--- src/components/player/player.tsx
+++ src/components/player/player.tsx
@@ -33,7 +33,7 @@ export const Player = () => {
       src='https://www.youtube.com/embed/M7FIvfx5J10?controls=0&showinfo=0&rel=0&autoplay=1&enablejsapi=1'
       title=' '
       allow='accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture;'
-      allowFullScreen
-    ></iframe>
+      allowFullScreen      
+    />
   );
 };
