34b534cd5460e07133ecd5fe754c447581dd3f1b
Maksim Nikolaev <mksniklaev@gmail.com>
2023-10-23 00:30:36 +0500

    fix: изменил протокол API на https

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,5 @@
 export const ACCESS_KEY = '6f7b568b5a945004ac6c9b9fbb5937fb';
-export const API = 'http://apilayer.net/api/validate';
+export const API = 'https://apilayer.net/api/validate';
 
 export const buttonValues = [
   '1',
